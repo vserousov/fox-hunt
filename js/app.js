fd06c021deb3ed342e415b2f4f27053abbf988f9
jackiemomo <dreki.li@mail.ru>
2016-07-25 12:00:52 +0300

    Added a game js-object

--- js/app.js
+++ js/app.js
@@ -17,3 +17,87 @@
 		}
 	});
 }());
+
+
+const game = function() {
+	var interval;
+	var dev_id;
+	var game_id;
+	
+	function generate_id() {
+		words = ["seoul", "moscow", "montreal", "dublin"];
+		
+		const id = words[Math.floor(Math.random() * 4)] + Math.floor(Math.random() * 1000);
+		
+		console.log(id)
+		return id;
+	}
+	
+	function init() {
+		dev_id = generate_id();
+		
+		$.ajax({
+			type: 'POST',
+			data: {
+				device_id: 3
+			},
+			url: "http://83.69.213.178:8082/init",
+			dataType: 'json',
+			success: function(response) {
+				console.log("Init response received: " + response.game_id);
+				
+				game_id = response.game_id;
+				setupUpdates()
+			}
+		});
+		
+		console.log("Init request sent")
+	}
+	
+	function errorHandler(err) {
+		console.log(err);
+        $("distance-text").val("CALAMITY!");
+	}
+	
+	function setupUpdates() {
+		if (navigator.geolocation) {
+	        WATCH_ID = navigator.geolocation.watchPosition(function(pos) {
+	        	$.ajax({
+	    			type: 'POST',
+	    			url: "http://83.69.213.178:8082/check_point",
+	    			dataType: 'json',
+	    			data: {
+	    				game_id: game_id,
+	    				point: pos.coords
+	    			},
+	    			success: handleUpdate
+	    		});
+	        	
+	        	console.log("Update request sent");
+	        }, errorHandler);
+	    } else {
+	        errorHandler("geolocation not found");
+	    }	
+	}
+	
+	function handleUpdate(response) {
+		console.log("Update response received: " + response.status);
+		
+		if (response.status == "ok") {
+			$("#distance-text").text(response.distance);
+		}
+		else {
+			$("#distance-text").text(response.status);
+		}
+	}
+		
+	return {
+		start : function () {
+			init();
+		},
+	
+		stop : function () {
+			clearInterval(interval);
+		}
+	}
+}()
